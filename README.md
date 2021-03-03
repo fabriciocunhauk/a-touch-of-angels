4bc82bc263a727c4a00cb935a963126d2b1abe4a
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-03-03 02:26:19 +0000

    starter layout

---
+++ README.md
@@ -0,0 +1,3 @@
+### eat-smart-page https://fabriciocunhauk.github.io/eat-smart-page
+
+<img src="./assets/images/frontpage.jpg" alt="frontpage" />
---
+++ assets/scripts/app.js
@@ -0,0 +1,57 @@
+const homeH1 = document.querySelector('.home-section-h1');
+const textString = homeH1.textContent;
+const splitLetters = textString.split("");
+
+homeH1.textContent = "";
+console.log(splitLetters);
+
+for (let i = 0; i < splitLetters.length; i++) {
+    if (splitLetters[i] === " ") {
+        homeH1.innerHTML += splitLetters[i]
+    }
+    homeH1.innerHTML += '<span>' + splitLetters[i] + '</span>'
+}
+
+let char = 0;
+let timer = setInterval(onTick, 20);
+
+function onTick() {
+    const span = homeH1.querySelectorAll('span')[char];
+    span.classList.add('fade')
+    char++
+    if (char === splitLetters.length) {
+        complete();
+        return
+    }
+};
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
+
+const burgerMenu = document.getElementById('burger-menu');
+const sideMenu = document.getElementById('menu-container');
+const burgerMenuDiv = document.querySelector('.burger-menu-div');
+
+burgerMenu.addEventListener('click', handleMenu);
+
+let menuEvent = false;
+
+function handleMenu() {
+    if (!menuEvent) {
+        menuEvent = true;
+        menuOpen();
+    } else {
+        menuClose();
+        menuEvent = false;
+    }
+};
+
+function menuOpen() {
+    burgerMenuDiv.style.width = '300px';
+};
+
+function menuClose() {
+    burgerMenuDiv.style.width = '0';
+};
